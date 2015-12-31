33e7003b350cb26d99031c5997198ff99fd01e78
nippled <nippled@linuxmail.org>
2015-12-31 19:14:54 +0100

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -276,8 +276,8 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 33,
-        SCRIPT_ADDRESS = 28,
+        PUBKEY_ADDRESS = 90,
+        SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
     };
